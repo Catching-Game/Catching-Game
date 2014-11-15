edf6a6a1c5238834fc4f45838048f5969d91057c
SimonHoinkis <Simon.Hoinkis@gmx.net>
2014-11-15 17:28:52 +0100

    Hintergrund wurde erstellt
    test

---
+++ KPROG-Projekt/Schachgesicht.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Schachgesicht here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Schachgesicht extends World
+{
+
+    /**
+     * Constructor for objects of class Schachgesicht.
+     * 
+     */
+    public Schachgesicht()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
