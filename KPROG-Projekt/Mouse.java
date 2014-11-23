6cd77a8b6d95ba9fcb2aa4603b4c7b6613992359
JRehder <Heiko@MacBook-Pro.fritz.box>
2014-11-23 16:42:07 +0100

    Mouse
    speed hinzugefügt

--- KPROG-Projekt/Mouse.java
+++ KPROG-Projekt/Mouse.java
@@ -1,17 +1,27 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Mouse here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * this class represents our Mouse in the game
+ * @author heikorehder
+ * @version 0.1 / 23.11.2014
  */
 public class Mouse extends Actor
 {
-    /**
-     * Act - do whatever the Mouse wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+    private int speed;
+    
+    /*
+     * init all variables of the actor
      */
+    
+    public Mouse(){
+    	init():
+    }
+    
+
+    public void init(){
+    	this.speed = XML_Gamelogic_Reader.getPlayerSpeed();
+    }
+    
     public void act() 
     {
         // Add your action code here.
