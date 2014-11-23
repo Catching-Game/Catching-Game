c581a6f4a5d8f0a5a825992e14deabc653e77251
SeeuD <Maxi-z350@gmx.de>
2014-11-23 21:50:01 +0100

    Revert "Mouse"
    This reverts commit a01374351796a2fc8ea2a1cfce4f3cc703e51995.

--- KPROG-Projekt/GameWorld.java
+++ KPROG-Projekt/GameWorld.java
@@ -39,7 +39,6 @@ public class GameWorld extends World
     	this.setWalls();
     	this.setPlayer();
     	this.setTrees();
-    	this.setMice();
     }
     
     private void setWalls()
@@ -59,19 +58,6 @@ public class GameWorld extends World
     	//TODO ID System zum Test wurde nur die PlayerID.First eingefuegt
     }
     
-    /**
-     * Set up Mouse Image from xml
-     */
-    private void setMice() 
-    {
-    	for(int zaehler = 0;zaehler < XML_Map_Reader.getMiceCount(); zaehler = zaehler + 1)
-    	{
-    		int posY = XML_Map_Reader.getMicePositions().get(zaehler).getY();
-    		int posX = XML_Map_Reader.getMicePositions().get(zaehler).getX();
-    		int cellSize = this.getCellSize();
-    		this.addObject(new Mouse(cellSize), posX, posY);
-    	}
-    }
     /**
      * Set up Tree Image from xml
      */
--- KPROG-Projekt/Mouse.java
+++ KPROG-Projekt/Mouse.java
@@ -1,45 +1,52 @@
-import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * this class represents our mouse in the game
- * 
  * @author heikorehder
  * @version 0.1 / 23.11.2014
  */
-public class Mouse extends Actor {
-	private int speed;
-	private int lifeIncrease;
-	private int sightRange;
+public class Mouse extends Actor
+{
+    private int speed;
+    private int lifeIncrease;
+    private int sightRange;
+ 
+    
+<<<<<<< HEAD
+    public Mouse(int cellSize){
+    	init(cellSize);
+=======
+    /*
+     * init all variables of the actor
+     */
+    
+    public Mouse(){
+    	init();
+>>>>>>> FETCH_HEAD
+    }
+    
+    /**
+     * init all variables of the actor.
+     */
 
-	public Mouse(int cellSize) {
-		init(cellSize);
-		act();
-	}
-
-	/**
-	 * init all variables of the actor.
-	 */
-
-	public void init(int cellSize) {
-		this.speed = XML_Gamelogic_Reader.getPlayerSpeed();
-		this.lifeIncrease = XML_Gamelogic_Reader.getMouseLifeIncrease();
-		this.sightRange = XML_Gamelogic_Reader.getMouseLifeIncrease();
-		this.setUpImage(cellSize);
-	}
-
+    public void init(int cellSize){
+    	this.speed = XML_Gamelogic_Reader.getPlayerSpeed();
+    	this.lifeIncrease = XML_Gamelogic_Reader.getMouseLifeIncrease();
+    	this.sightRange = XML_Gamelogic_Reader.getMouseLifeIncrease();
+    	this.setUpImage(cellSize);
+    }
+    
 	/**
 	 * Sets up the Image of the mouse
-	 * 
 	 * @param cellSize
 	 */
 	private void setUpImage(int cellSize) {
-		GreenfootImage image = new GreenfootImage(XML_Map_Reader.getMiceImage());
-		image.scale(cellSize, cellSize);
-		this.setImage(image);
-
-	}
-	
-	public void act(){
-		
-	}
+    	GreenfootImage image = new GreenfootImage(XML_Map_Reader.getMiceImage());
+    	image.scale(cellSize, cellSize);
+    	this.setImage(image);   
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
 }
