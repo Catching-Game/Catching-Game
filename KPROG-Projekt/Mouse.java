04f6d978c03e7eeca79fad9fbf44e23ab7985162
JRehder <Heiko@MacBook-Pro.fritz.box>
2014-11-23 17:07:57 +0100

    Mouse
    mouse image added

--- KPROG-Projekt/Mouse.java
+++ KPROG-Projekt/Mouse.java
@@ -1,26 +1,40 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * this class represents our Mouse in the game
+ * this class represents our mouse in the game
  * @author heikorehder
  * @version 0.1 / 23.11.2014
  */
 public class Mouse extends Actor
 {
     private int speed;
+    private int lifeIncrease;
+    private int sightRange;
+ 
     
-    /*
-     * init all variables of the actor
+    public Mouse(int cellSize){
+    	init(cellSize);
+    }
+    
+    /**
+     * init all variables of the actor.
      */
-    
-    public Mouse(){
-    	init():
-    }
-    
 
-    public void init(){
+    public void init(int cellSize){
     	this.speed = XML_Gamelogic_Reader.getPlayerSpeed();
+    	this.lifeIncrease = XML_Gamelogic_Reader.getMouseLifeIncrease();
+    	this.sightRange = XML_Gamelogic_Reader.getMouseLifeIncrease();
+    	this.setUpImage(cellSize);
     }
+    
+	/**
+	 * Sets up the Image of the mouse
+	 * @param cellSize
+	 */
+	private void setUpImage(int cellSize) {
+    	GreenfootImage image = new GreenfootImage(XML_Map_Reader.getMiceImage());
+    	image.scale(cellSize, cellSize);
+    	this.setImage(image);   
     
     public void act() 
     {
