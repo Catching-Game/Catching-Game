5b18c6ea4fc1ecc9e61e7e2a75a6415182de8208
SeeuD <Maxi-z350@gmx.de>
2014-11-23 16:09:07 +0100

    Work on Tree

--- KPROG-Projekt/GameWorld.java
+++ KPROG-Projekt/GameWorld.java
@@ -34,24 +34,42 @@ public class GameWorld extends World
         this.setBackground(XML_Map_Reader.getWorldBackgroundImage());
     }
     
-    private void setUpWorld() {
+    private void setUpWorld() 
+    {
     	this.setWalls();
     	this.setPlayer();
+    	this.setTrees();
     }
     
     private void setWalls()
     {
     	//Iteration to set up the walls
-    	for (int i = 0; i < XML_Map_Reader.getWallCount(); i++) {
-    		this.addObject(new Wall(this.getCellSize()), XML_Map_Reader.getWallPositions().get(i).getX(), 
+    	for (int i = 0; i < XML_Map_Reader.getWallCount(); i = i + 1) {
+    		this.addObject(new Wall(this.getCellSize()), 
+    				XML_Map_Reader.getWallPositions().get(i).getX(), 
     										XML_Map_Reader.getWallPositions().get(i).getY());
     	}
     }
     
-    private void setPlayer() {	
+    private void setPlayer() 
+    {	
     	this.addObject(new Player(this.getCellSize()), XML_Map_Reader.getPlayerPosition(PlayerID.FIRST).getX(),
     								XML_Map_Reader.getPlayerPosition(PlayerID.FIRST).getY());
     	//TODO ID System zum Test wurde nur die PlayerID.First eingefuegt
+    }
+    
+    /**
+     * Set up Tree Image from xml
+     */
+    private void setTrees()
+    {
+    	for(int zaehler = 0;zaehler < XML_Map_Reader.getTreeCount(); zaehler = zaehler + 1)
+    	{
+    		int posY = XML_Map_Reader.getTreePositions().get(zaehler).getY();
+    		int posX = XML_Map_Reader.getTreePositions().get(zaehler).getX();
+    		int cellSize = this.getCellSize();
+    		this.addObject(new Tree(cellSize), posX, posY);
+    	}
     }
     
 }
