b8d41289b6fa22e16e45b346d14f6cc8fc94b8c2
JRehder <Heiko@MacBook-Pro.fritz.box>
2014-11-24 23:48:02 +0100

    Mouse
    Mouse random isAtEdge

--- KPROG-Projekt/Mouse.java
+++ KPROG-Projekt/Mouse.java
@@ -1,5 +1,5 @@
 import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.Random;
 /**
  * this class represents our mouse in the game
  * 
@@ -12,9 +12,10 @@ public class Mouse extends Actor {
 	private int sightRange;
 	private Direction wantedDir;
 
+
 	public Mouse(int cellSize, Direction dir) {
-		init(cellSize,dir);
-		
+		init(cellSize, dir);
+
 	}
 
 	/**
@@ -26,10 +27,6 @@ public class Mouse extends Actor {
 		this.lifeIncrease = XML_Gamelogic_Reader.getMouseLifeIncrease();
 		this.sightRange = XML_Gamelogic_Reader.getMouseSightRange();
 		this.wantedDir = dir;
-		
-		
-		
-		
 		this.setUpImage(cellSize);
 	}
 
@@ -44,73 +41,150 @@ public class Mouse extends Actor {
 		this.setImage(image);
 
 	}
-	
-	public void act(){
-		//input();
+
+	public void act() {
+		// input();
 		move();
 	}
-	
-  
 
-	
-    private void move() {
-    	if(this.canWalkUP() && this.wantedDir == Direction.UP) {
-    		this.setLocation(this.getX(), this.getY() - this.speed);
-    	} else if(this.canWalkDOWN() && this.wantedDir == Direction.DOWN) {
-    		this.setLocation(this.getX(), this.getY() + this.speed);
-    	} else if(this.canWalkLEFT() && this.wantedDir == Direction.LEFT) {
-    		this.setLocation(this.getX() - this.speed, this.getY());
-    	} else if(this.canWalkRIGHT() && this.wantedDir == Direction.RIGHT) {
-    		this.setLocation(this.getX() + this.speed, this.getY());
-    	}
-    }
-    
-	
-    /**
-     * Checks if mouse can walk up
-     * @return true if yes, no if not
-     */
-    private boolean canWalkUP() {
-    	if(this.getOneObjectAtOffset(0, -speed, Wall.class) != null) {
-    		return false;
-    	}
-    	
-    	return true;
-    }
+	private void move() {
+		if (this.canWalkUP() && this.wantedDir == Direction.UP) {
+			walkUP();
+			if (isAtEdge()) {
+				if (randomDir() == 0) {
+					walkDOWN();
+				} else if (randomDir() == 1) {
+					walkLEFT();
+				} else if (randomDir() == 2) {
+					walkRIGHT();
+				}
+				else if (randomDir() == 3){
+					walkUP();
+				}
+			}
+		}
 
-    /**
-     * Checks if mouse can walk down
-     * @return true if yes, no if not
-     */
-    private boolean canWalkDOWN() {
-    	if(this.getOneObjectAtOffset(0, +speed, Wall.class) != null) {
-    		return false;
-    	}
-    	
-    	return true;  	
-    }
-    
-    /**
-     * Checks if mouse can walk left
-     * @return true if yes, no if not
-     */
-    private boolean canWalkLEFT() {
-    	if(this.getOneObjectAtOffset(-speed, 0, Wall.class) != null) {
-    		return false;
-    	}
-    	
-    	return true;
-    }
-    
-    /**
-     * Checks if mouse can walk right
-     * @return true if yes, no if not
-     */
-    private boolean canWalkRIGHT() {
-    	if(this.getOneObjectAtOffset(+speed, 0, Wall.class) != null) {
-    		return false;
-    	}
-    	
-    	return true;
-    }
+		
+		 else if (this.canWalkDOWN() && this.wantedDir == Direction.DOWN) {
+			walkDOWN();
+			if (isAtEdge()) {
+				if (randomDir() == 0) {
+					walkDOWN();
+				} else if (randomDir() == 1) {
+					walkLEFT();
+				} else if (randomDir() == 2) {
+					walkRIGHT();
+				}
+				else if (randomDir() == 3){
+					walkUP();
+				}
+			}
+		}
+			
+		 else if (this.canWalkLEFT() && this.wantedDir == Direction.LEFT) {
+			walkLEFT();
+			if (isAtEdge()) {
+				if (randomDir() == 0) {
+					walkDOWN();
+				} else if (randomDir() == 1) {
+					walkLEFT();
+				} else if (randomDir() == 2) {
+					walkRIGHT();
+				}
+				else if (randomDir() == 3){
+					walkUP();
+				}
+			}
+		}
+		 else if (this.canWalkRIGHT() && this.wantedDir == Direction.RIGHT) {
+			walkRIGHT();
+			if (isAtEdge()) {
+				if (randomDir() == 0) {
+					walkDOWN();
+				} else if (randomDir() == 1) {
+					walkLEFT();
+				} else if (randomDir() == 2) {
+					walkRIGHT();
+				}
+				else if (randomDir() == 3){
+					walkUP();
+				}
+		}
+
+	}
+	}
+
+	/**
+	 * Checks if mouse can walk up
+	 * 
+	 * @return true if yes, no if not
+	 */
+	private boolean canWalkUP() {
+		if (this.getOneObjectAtOffset(0, -speed, Wall.class) != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Checks if mouse can walk down
+	 * 
+	 * @return true if yes, no if not
+	 */
+	private boolean canWalkDOWN() {
+		if (this.getOneObjectAtOffset(0, +speed, Wall.class) != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Checks if mouse can walk left
+	 * 
+	 * @return true if yes, no if not
+	 */
+	private boolean canWalkLEFT() {
+		if (this.getOneObjectAtOffset(-speed, 0, Wall.class) != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Checks if mouse can walk right
+	 * 
+	 * @return true if yes, no if not
+	 */
+	private boolean canWalkRIGHT() {
+		if (this.getOneObjectAtOffset(+speed, 0, Wall.class) != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private void walkUP() {
+		this.setLocation(this.getX(), this.getY() - this.speed);
+	}
+
+	private void walkDOWN() {
+		this.setLocation(this.getX(), this.getY() + this.speed);
+	}
+
+	private void walkLEFT() {
+		this.setLocation(this.getX() - this.speed, this.getY());
+	}
+
+	private void walkRIGHT() {
+		this.setLocation(this.getX() + this.speed, this.getY());
+	}
+
+	public int randomDir() {
+		Random random = new Random();
+		return random.nextInt(3);
+
+	}
 }
