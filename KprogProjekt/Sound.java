ebc4e8e0297192960b79bbb68d55d23d69116ec9
Dscheksn <matthias.miguelkoehler@hotmail.de>
2014-12-02 14:20:34 +0100

    Mp3 Player

--- KprogProjekt/Sound.java
+++ KprogProjekt/Sound.java
@@ -1,30 +1,64 @@
+
+
 import greenfoot.GreenfootSound;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author Matthias Köhler
  */
-public class Sound
-{
-  /**
-   * The current soundtrack
-   */
-  private GreenfootSound soundtrack;
-  /**
-   * Constructor of class Sound.
-   */
-  public Sound()
-  {
-    createSound();
-//    soundtrack.playLoop();
-    System.out.println("Sound created and loop started.Have fun!");
-  }
+public class Sound {
 
-  /**
-   * Creates a sound for greenfoot from the folder sounds.
-   */
-  private void createSound()
-  {
-    GreenfootSound gs = new GreenfootSound("sounds/cantina.mp3");
-    this.soundtrack = gs;
-  }
+    /**
+     * The current soundtrack
+     */
+    public GreenfootSound soundtrack;
+
+    /**
+     * Button to stop the song
+     */
+    private Button buttonStop;
+
+    /**
+     * Constructor of class Sound.
+     */
+    public Sound() {
+        createSound();
+        soundtrack.playLoop();
+    }
+
+    /**
+     * Creates a sound for greenfoot from the folder sounds.
+     */
+    private void createSound() {
+        GreenfootSound gs = new GreenfootSound("sounds/cantina.mp3");
+        this.soundtrack = gs;
+    }
+    /**
+     * Change your current Song -- change to XML
+     */
+    public void changeSong(String s)
+    {
+        if(s == "Gummibärenbande")
+        {
+            GreenfootSound gs = new GreenfootSound("sounds/gummibären.mp3");
+            this.soundtrack = gs;
+            this.soundtrack.playLoop();
+        }
+        if(s == "Cantinaband")
+        {
+            GreenfootSound gs = new GreenfootSound("sounds/cantina.mp3");
+            this.soundtrack = gs;
+            this.soundtrack.playLoop();
+        }
+        if(s == "I3est Song")
+        {
+            GreenfootSound gs = new GreenfootSound("sounds/fantasy.mp3");
+            this.soundtrack = gs;
+            this.soundtrack.playLoop();
+        }
+        
+    }
 }
--- KprogProjekt/TheFrame.java
+++ KprogProjekt/TheFrame.java
@@ -1,10 +1,14 @@
+
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -12,116 +16,191 @@ import javax.swing.JLabel;
 
 public class TheFrame {
 
-	private JFrame myFrame;
-	private JButton loadGame;
-	private JButton newGame;
-	private JLabel comboBoxName;
-	private JComboBox<Integer> combo;
-	private Container contentPane;
-	private Integer[] inhalt = {2,3,4};
-	private int playerquantity;
-	private boolean isNewGame;
-	private boolean isLoadGame;
+    private JFrame myFrame;
+    private JButton loadGame;
+    private JButton newGame;
+    private JButton buttonMusic;
+    private JLabel comboBoxName;
+    private JLabel comboBoxLabelMusic;
+    private JComboBox<Integer> combo;
+    private JComboBox<String> comboMusic;
+    private Container contentPane;
+    private Integer[] inhalt = {2, 3, 4};
+    private String[] songs;
+    private int playerquantity;
+    private boolean isNewGame;
+    private boolean isLoadGame;
+    private boolean buttonPressed;
+    Sound s;
 
-	public TheFrame(){
-		init();
-	}
+    /**
+     * This icon represents the stop.gif of buttonMusic inside our frame.
+     */
+    final Icon STOP_ICON = new ImageIcon("images/stop128x128.gif");
+    /**
+     * This icon represents the play.gif of buttonMusic inside our frame.
+     */
+    final Icon PLAY_ICON = new ImageIcon("images/play128x128.gif");
 
-	private void init(){
-		//Initial Elements
-		//SetLoadGameButton
-		loadGame = new JButton();
-		loadGame.setText("Begonnenes Spiel fortsetzen");
-		loadGame.setAlignmentX(Component.LEFT_ALIGNMENT);
-		loadGame.setSize(200, 100);
-		loadGame.addActionListener(new ButtonListener());
-		//Set NewGameButton
-		newGame = new JButton();
-		newGame.setText("Neues Spiel mit deinen Freunden beginnen");
-		newGame.setAlignmentX(Component.LEFT_ALIGNMENT);
-		newGame.setSize(200,100);
-		newGame.addActionListener(new ButtonListener());
-		//Set Combo Box Label
-		comboBoxName = new JLabel();
-		comboBoxName.setText("Spieleranzahl");
-		comboBoxName.setAlignmentX(Component.LEFT_ALIGNMENT);
-		//Set Combo Box
-		combo = new JComboBox<Integer>();
-		combo.setSize(50, 50);
-		combo.setAlignmentX(Component.LEFT_ALIGNMENT);
-		//Set Frame and Pane
-		myFrame = new JFrame();
-		contentPane = myFrame.getContentPane();
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+    public TheFrame() {
+        init();
+        s = new Sound();
+        s.soundtrack.setVolume(100);
+    }
 
-		//Add elements to ComboList
-		for(Integer inhalte: inhalt){
-			combo.addItem(inhalte);
-		}
+    private void init() {
+        //Initial Elements
+        //SetLoadGameButton
+        loadGame = new JButton();
+        loadGame.setText("Begonnenes Spiel fortsetzen");
+        loadGame.setAlignmentX(Component.LEFT_ALIGNMENT);
+        loadGame.setSize(200, 100);
+        loadGame.addActionListener(new ButtonListener());
+        //Set NewGameButton
+        newGame = new JButton();
+        newGame.setText("Neues Spiel mit deinen Freunden beginnen");
+        newGame.setAlignmentX(Component.LEFT_ALIGNMENT);
+        newGame.setSize(200, 100);
+        newGame.addActionListener(new ButtonListener());
+        //Create MediaPlayer Button
+        buttonMusic = new JButton(STOP_ICON);
+        buttonMusic.setToolTipText("By clicking on this button music will stop!");
+        buttonMusic.setForeground(Color.black);
+        buttonMusic.setBackground(Color.white);
+        buttonMusic.setContentAreaFilled(false);
+        buttonMusic.setOpaque(true);
+        buttonMusic.setText("Please stop this shit omg!");
+        buttonMusic.addActionListener(new ButtonListener());
+        //Set Combo Box Label
+        comboBoxName = new JLabel();
+        comboBoxName.setText("Spieleranzahl");
+        comboBoxName.setAlignmentX(Component.LEFT_ALIGNMENT);
+        //Set Combo Box
+        combo = new JComboBox<Integer>();
+        combo.setSize(50, 50);
+        combo.setAlignmentX(Component.LEFT_ALIGNMENT);
+        //Set Combo Box Label
+        comboBoxLabelMusic = new JLabel();
+        comboBoxLabelMusic.setText("Songliste");
+        comboBoxLabelMusic.setAlignmentX(Component.LEFT_ALIGNMENT);
+        //Set Combo Box with Songs
+        comboMusic = new JComboBox<String>();
+        comboMusic.setSize(50, 50);
+        comboMusic.setAlignmentX(Component.LEFT_ALIGNMENT);
+        //add items to the Songlist
+        comboMusic.addItem("Cantinaband");
+        comboMusic.addItem("Gummibärenbande");
+        comboMusic.addItem("I3est Song");
+        comboMusic.addActionListener(new ComboListener());
+       
 
-		//Add elements to the Layout
-		Panel functionPanel = new Panel();
-		contentPane.add(functionPanel, BorderLayout.CENTER);
-		contentPane.add(newGame);
-		contentPane.add(comboBoxName);
-		contentPane.add(combo);
-		contentPane.add(loadGame);
+        //Set Frame and Pane
+        myFrame = new JFrame();
+        contentPane = myFrame.getContentPane();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 
+        //Add elements to ComboList
+        for (Integer inhalte : inhalt) {
+            combo.addItem(inhalte);
+        }
 
-		//Set Frame size and Visible
-		myFrame.setSize(400,400);
-		myFrame.pack();
-		myFrame.setVisible(true);
+        //Add elements to the Layout
+        Panel functionPanel = new Panel();
+        contentPane.add(functionPanel, BorderLayout.CENTER);
+        contentPane.add(newGame);
+        contentPane.add(comboBoxName);
+        contentPane.add(combo);
+        contentPane.add(loadGame);
+        contentPane.add(buttonMusic);
+        contentPane.add(comboBoxLabelMusic);
+        contentPane.add(comboMusic);
 
-	} 
-	
-	private void setPlayerquantitiy(int quantitiy){
-		this.playerquantity = quantitiy;
-	}
-	
-	public int getPlayerquantity(){
-		return playerquantity;
-	}
-	
-	private void setIsNewGame(boolean newGame){
-		this.isNewGame = newGame;
-	}
-	
-	public boolean getIsNewGame(){
-		return isNewGame;
-	}
-	
-	public void setisLoadGame(boolean loadGame){
-		this.isLoadGame = loadGame;
-	}
-	
-	public boolean getIsLoadGame(){
-		return isLoadGame;
-	}
-	
-	/**
-	 * Action Listener Class for the Buttons
-	 * @author Max
-	 *
-	 */
-	private class ButtonListener implements ActionListener{
-		/**
-		 * Action Listener for the Buttons
-		 */
-		public void actionPerformed(ActionEvent arg0) {
-			if(arg0.getSource() == newGame){
-				System.out.println("Button neues Spiel gedueckt");
-				System.out.println(combo.getSelectedItem());
-				setPlayerquantitiy((Integer)combo.getSelectedItem());
-				setIsNewGame(true);
-				setisLoadGame(false);
-				
-			} else if(arg0.getSource() == loadGame){
-				System.out.println("Spiel Laden gedrueckt");
-				setIsNewGame(false);
-				setisLoadGame(true);
-			}
+        //Set Frame size and Visible
+        myFrame.setSize(310, 300);
+        //myFrame.pack();
+        myFrame.setVisible(true);
 
-		}
-	}
+        //This part is for the Song
+    }
+
+    private void setPlayerquantitiy(int quantitiy) {
+        this.playerquantity = quantitiy;
+    }
+
+    public int getPlayerquantity() {
+        return playerquantity;
+    }
+
+    private void setisNewGame(boolean newGame) {
+        this.isNewGame = newGame;
+    }
+
+    public boolean getisNewGame() {
+        return isNewGame;
+    }
+
+    public void setisLoadGame(boolean loadGame) {
+        this.isLoadGame = loadGame;
+    }
+
+    public boolean getisLoadGame() {
+        return isLoadGame;
+    }
+
+    /**
+     * Action Listener Class for the Buttons
+     *
+     * @author Max
+     * @author modified Matthias Köhler
+     *
+     */
+    private class ButtonListener implements ActionListener {
+
+        /**
+         * Action Listener for the Buttons
+         */
+        public void actionPerformed(ActionEvent arg0) {
+            if (arg0.getSource() == newGame) {
+                System.out.println("Button neues Spiel gedueckt");
+                System.out.println(combo.getSelectedItem());
+                setPlayerquantitiy((Integer) combo.getSelectedItem());
+                setisNewGame(true);
+                setisLoadGame(false);
+
+            } else if (arg0.getSource() == loadGame) {
+                System.out.println("Spiel Laden gedrueckt");
+                setisNewGame(false);
+                setisLoadGame(true);
+            } else if (arg0.getSource() == buttonMusic && buttonPressed != true) {
+                //stop the music
+                s.soundtrack.pause();
+                buttonPressed = true;
+                buttonMusic.setIcon(PLAY_ICON);
+                buttonMusic.setText("Play!                                ");               
+            } else if (arg0.getSource() == buttonMusic && buttonPressed == true) {
+                //play music again
+                s.soundtrack.playLoop();
+                buttonMusic.setIcon(STOP_ICON);
+                buttonPressed = false;
+                buttonMusic.setText("Please stop this shit omg!");
+            }
+
+        }
+    }
+    
+    /**
+     * Action Listener Class for Combo Box
+     * @author Matthias Köhler
+     */
+    private class ComboListener implements ActionListener 
+    {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JComboBox cb = (JComboBox) e.getSource();
+            String songName = (String) cb.getSelectedItem();
+            s.soundtrack.stop();
+            s.changeSong(songName);
+        }
+        
+    }
 }
