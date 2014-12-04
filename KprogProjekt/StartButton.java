4fc53587902875d288bf266879959f9096102891
Dscheksn <matthias.miguelkoehler@hotmail.de>
2014-12-04 10:19:57 +0100

    added final variablen etc

--- KprogProjekt/StartButton.java
+++ KprogProjekt/StartButton.java
@@ -9,47 +9,52 @@ import greenfoot.Greenfoot;
  * @version (a version number or a date)
  */
 public class StartButton extends Actor
-{ 
-    protected int playerCount = 1; // at least one player
-    private final int ONEPLAYER = 1;
-    private final int TWOPLAYERS = 2; 
-    private final int THREEPLAYERS = 3;
-    private final int FOURPLAYERS = 4;
-    public StartButton(int playerCount)
-    {
-        this.playerCount = playerCount;
-    }
+{
+
+  protected int playerCount = 1; // at least one player
+  private final int ONEPLAYER = 1;
+  private final int TWOPLAYERS = 2;
+  private final int THREEPLAYERS = 3;
+  private final int FOURPLAYERS = 4;
+
+  public StartButton(int playerCount)
+  {
+    this.playerCount = playerCount;
+  }
+
   /**
-   * An example of a method - replace this comment with your own
+   * An example of a method - replace this comment with your own.
    *
    * @param y a sample parameter for a method
    * @return the sum of x and y
    */
   public void act()
   {
+    System.out.println(Greenfoot.getMouseInfo().getButton());
     if (Greenfoot.mouseClicked(this))
     {
-      if (playerCount == 1)
+      if (playerCount == TWOPLAYERS)
       {
-          System.out.println("One Player");
-        }
-        if (playerCount == 2)
-        {
-            System.out.println("Two Player");
-        }
-        if (playerCount == 3)
-        {
-            System.out.println("Three PLayer");
-        }
+        System.out.println("Two Player");
+      }
+      if (playerCount == THREEPLAYERS)
+      {
+        System.out.println("Three PLayer");
+      }
+      if (playerCount == FOURPLAYERS)
+      {
+        System.out.println("Four Player");
+      }
     }
-    
-    //if(Greenfoot.mouseClicked(twoPlayerButton))
-    //{
-    //  System.out.println("TwoPlayerButton");
-   // }
   }
 
-  public static StartButton createButton(int playerCount)
+  /**
+   * Create a Startbutton.
+   *
+   * @param playerCount the number of players
+   * @return return the button with number of players
+   */
+  public static StartButton createButton(final int playerCount)
   {
     return new StartButton(playerCount);
   }
